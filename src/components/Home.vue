2042db612618c293db51678b5e1f31f269137c53
Jawad Dhew <jawaddhew98@gmail.com>
2023-12-25 16:37:30 +0600

    final

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,22 @@
 <template>
 
-<h1>Home</h1>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+   
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item mx-5"><button type="button" class="btn btn-info"><a class="nav-link active" aria-current="page" href="/signup">Signup</a></button>
+          
+        </li>
+        <li class="nav-item"><button type="button" class="btn btn-info"><a class="nav-link active" href="/login">Log In</a></button>
+          
+        </li>
+        
+       
+      </ul>
+    </div>
+  </div>
+</nav>
 </template>
 
 <script>
